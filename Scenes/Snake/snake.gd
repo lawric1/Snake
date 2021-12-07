d15f2be8717abdb6bee1cf1dc6eaf9e3f8f8a725
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-12-07 10:46:49 -0300

    Fix bugs in movement, Add food and growth, Add death

--- Scenes/Snake/snake.gd
+++ Scenes/Snake/snake.gd
@@ -1,5 +1,17 @@
 extends KinematicBody2D
 
+var cellScene = preload("res://Scenes/Snake/Cell.tscn")
+
+var foodScene = preload("res://Scenes/Food/Food.tscn")
+var food
+var foodPosition
+var isFoodSpawned = false
+
+const WIDTH = 160
+const HEIGHT = 96
+
+var rng = RandomNumberGenerator.new()
+
 var canMove = true
 
 var tileSize = 8
@@ -12,7 +24,6 @@ var direction = right
 
 var cellList
 var nextPositions = []
-var nextHeadPosition
 
 
 func _ready():
@@ -20,7 +31,8 @@ func _ready():
 
 	for cell in cellList:
 		nextPositions.append(cell.position)
-	
+
+
 func _physics_process(_delta):
 	if Input.is_action_pressed("ui_up") and direction != down:
 		direction = up
@@ -31,38 +43,43 @@ func _physics_process(_delta):
 	elif Input.is_action_pressed("ui_right") and direction != left:
 		direction = right
 	
-	if canMove:
+	if canMove and direction:
 		move()
+		eatFood()
+		colorSnake()
 		
 		canMove = false
 		yield(get_tree().create_timer(0.2), "timeout")
 		canMove = true
+	
+	spawnFood()
+
 
 func updateNextPositions():
-	var screenSize = OS.get_window_size()
-	var width = screenSize[0] / 4
-	var height = screenSize[1] / 4
-	
 	nextPositions.append(nextPositions[-1] + direction)
 	
-	
 	if nextPositions[-1].x < 0:
-		nextPositions[-1].x = width - 8
+		nextPositions[-1].x = WIDTH
 		
-	if nextPositions[-1].x >= width:
+	elif nextPositions[-1].x >= WIDTH:
 		nextPositions[-1].x = 0
 		
-	if nextPositions[-1].y < 0:
-		nextPositions[-1].y = height - 8
+	elif nextPositions[-1].y < 0:
+		nextPositions[-1].y = HEIGHT
 		
-	if nextPositions[-1].y >= height:
+	elif nextPositions[-1].y >= HEIGHT:
 		nextPositions[-1].y = 0
 	
-		
-	nextPositions.remove(0)
 	
-	
+	if nextPositions.size() > cellList.size() + 1:
+		nextPositions.remove(0)
+
+
 func move():
+	var currentPositions = []
+	for cell in cellList:
+		currentPositions.append(cell.position)
+		
 	updateNextPositions()
 	
 	var index = 1
@@ -70,8 +87,48 @@ func move():
 		cell.position = nextPositions[-index]
 
 		index += 1
+
+	if currentPositions.has(nextPositions[-1]):
+			print('died')
+			get_tree().paused = true
+
+
+func increaseSize():
+	var cell = cellScene.instance()
+	cell.add_to_group("snakeBody")
+	cell.position = nextPositions[0]
+	self.add_child(cell)
 	
-	
-	print(nextPositions[-1])
-	
-	
+	cellList = get_tree().get_nodes_in_group("snakeBody")
+
+
+func spawnFood():
+	if not isFoodSpawned:
+		rng.randomize()
+		var x = rng.randi_range(0, 19) * 8
+		var y = rng.randi_range(0, 11) * 8
+		
+		foodPosition = Vector2(x, y)
+		print(foodPosition)
+		
+		food = foodScene.instance()
+		food.position = foodPosition
+		
+		get_parent().add_child(food)
+		
+		isFoodSpawned = true
+
+
+func eatFood():
+	if cellList[0].position == foodPosition:
+		food.queue_free()
+		
+		increaseSize()
+		isFoodSpawned = false
+
+
+func colorSnake():
+	var color = 1
+	for cell in cellList:
+		cell.get_node("Sprite").modulate = Color(color, color, color)
+		color += 0.1
